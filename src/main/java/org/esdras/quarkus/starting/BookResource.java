5fa2f30d1de3038e4a7989af256b2e786a60634b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-11-19 18:53:42 -0300

    feat:adding rest

--- src/main/java/org/esdras/quarkus/starting/BookResource.java
+++ src/main/java/org/esdras/quarkus/starting/BookResource.java
@@ -1,16 +1,41 @@
 package org.esdras.quarkus.starting;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.List;
+import java.util.Optional;
+
 @Path("/api/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "ESDRAS GATAO";
+    public List<Book> getBooks() {
+        return List.of(
+                new Book(1, "The Fellowship of the Ring", "J. R. R. Tolkien", 1954, "Fantasy"),
+                new Book(2, "The Two Towers", "J. R. R. Tolkien", 1954, "Fantasy"),
+                new Book(3, "The Return of the King", "J. R. R. Tolkien", 1955, "Fantasy")
+        );
     }
+
+
+    @GET
+    @Path("{id}")
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        return this.getBooks().stream().filter(book -> book.id == id).findFirst();
+    }
+
+    @GET
+    @Path("/count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int countBooks() {
+        return this.getBooks().size();
+    }
+
+
+
+
+
 }
